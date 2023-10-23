c268968281e71df2e3d7699cad0f9bd0380f869e
Alex Molde <alex@apphouse.co.za>
2023-10-23 12:00:01 +0200

    articles and endpoints test

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -3,14 +3,13 @@ package io.collective.articles;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.collective.restsupport.BasicHandler;
 import org.eclipse.jetty.server.Request;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArticlesController extends BasicHandler {
     private final ArticleDataGateway gateway;
-
     public ArticlesController(ObjectMapper mapper, ArticleDataGateway gateway) {
         super(mapper);
         this.gateway = gateway;
@@ -20,16 +19,22 @@ public class ArticlesController extends BasicHandler {
     public void handle(String target, Request request, HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
 
-            { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
+            List<ArticleInfo> articles = new ArrayList<>();
 
-            }
+            gateway.findAll().forEach(record -> articles.add(new ArticleInfo(record.getId(), record.getTitle())));
+
+            writeJsonBody(servletResponse, articles);
+
         });
 
         get("/available", List.of("application/json"), request, servletResponse, () -> {
 
-            { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
+            List<ArticleInfo> articles = new ArrayList<>();
 
-            }
+            gateway.findAvailable().forEach(record -> articles.add(new ArticleInfo(record.getId(), record.getTitle())));
+
+            writeJsonBody(servletResponse, articles);
+
         });
     }
 }
--- components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
+++ components/endpoints/src/main/java/io/collective/endpoints/EndpointWorker.java
@@ -2,10 +2,13 @@ package io.collective.endpoints;
 
 import io.collective.articles.ArticleDataGateway;
 import io.collective.restsupport.RestTemplate;
+import io.collective.rss.RSS;
 import io.collective.workflow.Worker;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 import java.io.IOException;
 
@@ -30,8 +33,7 @@ public class EndpointWorker implements Worker<EndpointTask> {
         String response = template.get(task.getEndpoint(), task.getAccept());
         gateway.clear();
 
-        { // todo - map rss results to an article infos collection and save articles infos to the article gateway
-
-        }
+        RSS rss = new XmlMapper().readValue(response, RSS.class);
+        rss.getChannel().getItem().forEach(item -> gateway.save(item.getTitle()));
     }
 }
